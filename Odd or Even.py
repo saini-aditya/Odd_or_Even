2f5f7052074fa97137b66f70da81ea36b0184032
Aditya Saini <aditya.saini7b2024@gmail.com>
2024-05-23 11:15:06 +0530

    Add files via upload

---
+++ Odd or Even.py
@@ -0,0 +1,12 @@
+# Odd or Even.py
+
+number = int(input("Enter a number: "))
+even_test = number//2
+
+if even_test == number/2 + 0.00:
+    print (" ")
+    print (number, "is an even number")
+
+else :
+    print (" ")
+    print (number, "is an odd number")
